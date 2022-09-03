f455290eb03b3ddec62a926270d1ac6fc0a51d02
dbuendia <buendia.barcelo@gmail.com>
2022-09-03 19:51:40 +0200

    Hack

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import Counter from "./components/Counter.jsx";
 import Header from "./components/Header.jsx";
 
-function App() {
-  function createCounters(number) {
-    let counterArray = [];
-    for (let i = 1; i <= number; i++) {
-      counterArray[i] = <Counter number={i} />;
-    }
-    return counterArray;
-  }
-  return (
-    <div className="App">
-      <Header />
-      <div className="counter-container">{createCounters(3)}</div>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
